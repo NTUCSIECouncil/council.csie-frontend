b7379904028eb2b5f95ae4cf6ea64c4a265c6167
ZoeTsou <zoe.tsou.2003@gmail.com>
2024-02-02 14:16:23 +0800

    Initial layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { type FC } from 'react';
+import styles from '@/styles/utils.module.css'
 
 const Page: FC = () => {
   return (
@@ -7,20 +8,32 @@ const Page: FC = () => {
       <div>
         <h1>The main page of CSIE website.</h1>
       </div>
-      <div>
+      <div className={styles.layout}>
         <div>
           <Link href="/life">
-            <button>德田生活</button>
+            <button className={styles.button}>
+              <div>
+                <h2>德田生活</h2>
+                <hr color="white" style={{ width: '80%' }}/>
+                <p className={styles.paragraph}>
+                國立臺灣大學（簡稱臺灣大學、臺大、NTU），1928年創立於臺灣臺北市，是臺灣第一所高等教育機構，為臺灣學生人數最多的大專院校[11][12]。前身為大日本帝國九所帝國大學之一的「臺北帝國大學」。
+                </p>
+              </div>
+            </button>
           </Link>
         </div>
         <div>
           <Link href="/rate">
-            <button>課程評價網</button>
+            <button className={styles.button}>
+              課程評價網
+            </button>
           </Link>
         </div>
         <div>
           <Link href="/database">
-            <button>課程資料庫</button>
+            <button className={styles.button}>
+              課程資料庫
+            </button>
           </Link>
         </div>
       </div>
