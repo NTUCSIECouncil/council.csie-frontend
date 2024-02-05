6b513a9bbf3528e72e287f4543b961e1041f84e7
Ming <shimeming@gmail.com>
2024-02-05 09:00:16 +0800

    style(front): remove unused font roboto
    Eliminate the import of the Google Font Roboto, previously included by
    Next.js initialization. This commit removes the code associated with
    the Roboto font in front/src/app/layout.tsx for better code cleanliness
    and to resolve related issues.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 'use client';
-import { Roboto } from 'next/font/google';
 import './globals.css';
-import NavBar from '../components/NavBar';
-import { AuthContextProvider } from '../context/AuthContext';
+import NavBar from '@/components/NavBar';
+import { AuthContextProvider } from '@/context/AuthContext';
 import { type FC, type ReactNode } from 'react';
 
-const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body>
         <AuthContextProvider>
           <NavBar />
           <div style={{ marginTop: '80px' }}>
