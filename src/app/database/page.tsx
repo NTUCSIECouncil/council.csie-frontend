be2328671726d7a4cea9dee6afbd56031ab5078b
Ming <shimeming@gmail.com>
2024-02-07 10:44:28 +0800

    fix(front): eliminate errors in database page
    Did the same thing as 29b005.
    Formatted the file.

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -13,41 +13,47 @@ const Page: FC = () => {
       {/* <div className={styles.courseLayout}> */}
       <div className={styles.wrap}>
         <div className={styles.search}>
-            <input type="text" className={styles.searchTerm} placeholder="關鍵字搜尋" />
-            <Link href="/database/filterResults">
+          <input type="text" className={styles.searchTerm} placeholder="關鍵字搜尋" />
+          <Link href="/database/filterResults">
             <button type="submit" className={styles.searchButton}>
               <i className="fa fa-search"></i>
             </button>
-            </Link>
+          </Link>
         </div>
-        <p className={styles.word}>篩選：&nbsp;&nbsp;&nbsp;
-          <select id="selectGrade" className={styles.selectBox}>
+        <p className={styles.word}>
+          篩選：&nbsp;&nbsp;&nbsp;
+          <select
+            defaultValue={'all'}
+            id="selectGrade"
+            className={styles.selectBox}
+          >
             <option value="all">年級</option>
             <option value="freshman">大一</option>
-            <option value="sophomore" selected>大二</option>
+            <option value="sophomore">大二</option>
             <option value="junior">大三</option>
             <option value="senior">大四</option>
           </select>
           &nbsp; &nbsp;
-          <select id="selectType" className={styles.selectBox}>
+          <select defaultValue='all' id="selectType" className={styles.selectBox}>
             <option value="all">分類</option>
             <option value="require">必修</option>
-            <option value="select" selected>選修</option>
+            <option value="select">選修</option>
           </select>
-          <div className={styles.customBreak}></div>
+        </p>
+        <p className={styles.word}>
           TAG：&nbsp;&nbsp;&nbsp;
-          <a href="/tagged/taxi" className={styles.tag}>計程</a>
-          <a href="/tagged/hard" className={styles.tag}>超硬</a>
-          <a href="/tagged/hw" className={styles.tag}>作業永遠寫不完</a>
+          <Link href="/tagged/taxi" className={styles.tag}>計程</Link>
+          <Link href="/tagged/hard" className={styles.tag}>超硬</Link>
+          <Link href="/tagged/hw" className={styles.tag}>作業永遠寫不完</Link>
         </p>
         <svg width="0" height="0" viewBox="0 0 400 300">
-            <defs>
-              <mask id="mask">
-                <rect fill="#000000" x="0" y="0" width="400" height="300"></rect>
-                <circle fill="#FFFFFF" cx="150" cy="150" r="100" />
-                <circle fill="#FFFFFF" cx="50" cy="50" r="150" />
-              </mask>
-            </defs>
+          <defs>
+            <mask id="mask">
+              <rect fill="#000000" x="0" y="0" width="400" height="300"></rect>
+              <circle fill="#FFFFFF" cx="150" cy="150" r="100" />
+              <circle fill="#FFFFFF" cx="50" cy="50" r="150" />
+            </mask>
+          </defs>
         </svg>
       </div>
     </main>
