affdd01ed867df69247c8e3860a4d027fa3822a6
thomasinfor <thomaswang2003@gmail.com>
2024-03-29 02:15:05 +0800

    fix(front): add type to components/FullScreen
    sorry linter 😅

--- src/components/FullScreen.tsx
+++ src/components/FullScreen.tsx
@@ -1,6 +1,7 @@
-import { type FC } from 'react';
+import { type FC, type ReactNode } from 'react';
+import type CSS from 'csstype';
 
-const FullScreen: FC = ({ children, style = {}, ...props }) => {
+const FullScreen: FC<{ children: ReactNode; style: CSS }> = ({ children, style = {}, ...props }) => {
   return (
     <div style={{ ...style, height: 'calc(100vh - 80px)', position: 'relative' }} {...props}>
       <div style={{ width: '100%', height: '100%', backgroundColor: 'var(--background)', position: 'absolute', zIndex: -100 }}></div>
