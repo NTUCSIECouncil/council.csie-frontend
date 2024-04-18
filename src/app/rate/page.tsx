f1eb8826a1a82b12b29f675b1382017a28479e84
Ji Kuai <rk427454171@gmail.com>
2024-04-18 23:17:14 +0800

    feat: modify tag to multiple chosen possible. Send category, grade, tags and keyword in URL params

--- src/app/rate/page.tsx
+++ src/app/rate/page.tsx
@@ -1,13 +1,60 @@
 'use client';
 // import Search from '@/app/ui/search';
 // import Image from 'next/image';
-import { type FC } from 'react';
+import React, { useState, type FC } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from '@/styles/rate.module.css';
 import FullScreen from '@/components/FullScreen';
+import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+
+function submitSearch(router: AppRouterInstance, grade: string, category: string, keyword: string, tags: any) {
+  const searchUrl = '/rate/filterResults';
+  const params: any = { };
+  
+  if (grade !== 'all')
+    params['grade'] = grade;
+  if (category !== 'all')
+    params['category'] = category;
+  if (keyword !== '')
+    params['keyword'] = keyword;
+
+  const chosenTags = [];
+  for (const k in tags) {
+    if (tags[k].state[0])
+      chosenTags.push(k);
+  }
+  if (chosenTags.length > 0) {
+    params['tags'] = chosenTags;
+  }
+
+  router.push(searchUrl + '?' + (new URLSearchParams(params)).toString());
+}
 
 const Page: FC = () => {
+  const router = useRouter();
+  const [grade, setGrade] = useState('all');
+  const [category, setCategory] = useState('all');
+  const [keyword, setKeyword] = useState('');
+  const availableTags = {
+    'tax': {
+      name: '計程',
+      state: useState(false),
+    },
+    'hard': {
+      name: '超硬',
+      state: useState(false),
+    },
+    'hw': {
+      name: '作業永遠寫不完',
+      state: useState(false),
+    }
+  };
+
+  const flipTag = (state: [boolean, React.Dispatch<React.SetStateAction<boolean>>]) => {
+    state[1](!state[0]);
+  };
+
   return (
     <FullScreen className={styles.bodyRate}>
       <Image className={styles.background} src="/building.jpg" width="1000" height="1000" alt="background" />
@@ -16,20 +63,19 @@ const Page: FC = () => {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       {/* <div className={styles.courseLayout}> */}
       <div className="searchWrap">
-        <input type="text" className="searchBox" placeholder="關鍵字搜尋" />
-        <Link href="/rate/filterResults">
-          <button type="submit" className="searchButton">
-            <i className="fa fa-search"></i>
-          </button>
-        </Link>
+        <input type="text" className="searchBox" placeholder="關鍵字搜尋" onChange={(e) => setKeyword(e.target.value)} />
+        <button type="submit" className="searchButton" onClick={() => submitSearch(router, grade, category, keyword, availableTags)}>
+          <i className="fa fa-search"></i>
+        </button>
       </div>
       <div className={styles.wrap}>
         <p className={styles.word}>
           篩選：&nbsp;&nbsp;&nbsp;
           <select
-            defaultValue="all"
+            defaultValue={grade}
             id="selectGrade"
             className={styles.selectBox}
+            onChange={(e) => setGrade(e.target.value)}
           >
             <option value="all">年級</option>
             <option value="freshman">大一</option>
@@ -38,7 +84,12 @@ const Page: FC = () => {
             <option value="senior">大四</option>
           </select>
           &nbsp; &nbsp;
-          <select defaultValue="all" id="selectType" className={styles.selectBox}>
+          <select
+            defaultValue={category}
+            id="selectType"
+            className={styles.selectBox}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="all">分類</option>
             <option value="require">必修</option>
             <option value="select">選修</option>
@@ -46,9 +97,14 @@ const Page: FC = () => {
         </p>
         <p className={styles.word}>
           TAG：&nbsp;&nbsp;&nbsp;
-          <Link href="/tagged/taxi" className="tag">計程</Link>
-          <Link href="/tagged/hard" className="tag">超硬</Link>
-          <Link href="/tagged/hw" className="tag">作業永遠寫不完</Link>
+          { Object.keys(availableTags).map((tag, idx) => (
+            <button
+              onClick={(e) => flipTag((availableTags as any)[tag].state)}
+              className={'tag' +  ((availableTags as any)[tag].state[0] ? ' tag-on' : '')}
+              style={{ cursor: 'pointer' }}
+              type="button"
+            >{(availableTags as any)[tag].name}</button>
+          ))}
         </p>
       </div>
     </FullScreen>
