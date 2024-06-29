45d7a8227ef9757fd1d1c07a4897329fd9cd49c3
shimeming <shimeming@gmail.com>
2024-06-29 16:58:38 +0800

    feat(quiz): display 404 when requesting failed

--- src/app/database/[quizID]/page.tsx
+++ src/app/database/[quizID]/page.tsx
@@ -12,6 +12,7 @@ import ImageIcon from '@mui/icons-material/Image';
 import PortraitIcon from '@mui/icons-material/Portrait';
 import { UserAuth } from '@/context/AuthContext';
 import { type UUID } from 'crypto';
+import { notFound } from 'next/navigation';
 
 interface Params {
   quizID: string;
@@ -28,16 +29,17 @@ interface Quiz {
 // const Page: FC<{ params: { quizID: string } }> = ({ params }) => {
 const Page: FC<{ params: Params }> = ({ params }) => {
   const { request } = UserAuth();
+  const [requestFail, setRequestFail] = useState<boolean>(false);
   const [src, setSrc] = useState<null | string>(null);
   const [title, setTitle] = useState<string>('title');
   const [course, setCourse] = useState<string>('course');
   const [semester, setSemester] = useState<string>('semester');
   const [author, setAuthor] = useState<string>('author');
-
+  // test url: 00000004-0001-0000-0000-000000000000
   useEffect(() => {
     (async () => {
       if (request !== undefined) {
-        const res = await request('/api/quizzes/00000004-0001-0000-0000-000000000000');
+        const res = await request('/api/quizzes/' + params.quizID);
         if (res === null) {
           throw Error('request error');
         } else if (!res.ok) {
@@ -45,73 +47,79 @@ const Page: FC<{ params: Params }> = ({ params }) => {
         } else {
           const result = (await res.json()).result as Quiz;
           setSrc(result.download_link);
-          console.log(result.download_link);
         }
       }
-    })().catch(err => { console.error(err); });
+    })().catch(err => {
+      setRequestFail(true);
+      console.error(err);
+    });
   }, []);
 
   console.log(params.quizID);
 
-  return (
-    <section className={styles.all}>
-      <section className={styles.title}>
-        {course + ' - ' + title}
-      </section>
-      <Stack direction="row" className={styles.attributes}>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <PortraitIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary="Author"
-            secondary={author}
-          />
-        </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <ImageIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary="Course"
-            secondary={course}
-          />
-        </ListItem>
-        {/* <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem> */}
-      </Stack>
-      <div className={styles.content}>
-        <div className={styles.open_in_new_tab_footnote}>
-          <Link
-            href="https://mk8d.wang.works/report.pdf"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Click here to open the file in a new tab.
-          </Link>
-        </div>
+  if (requestFail) {
+    return notFound();
+  } else {
+    return (
+      <section className={styles.all}>
+        <section className={styles.title}>
+          {course + ' - ' + title}
+        </section>
+        <Stack direction="row" className={styles.attributes}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <PortraitIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary="Author"
+              secondary={author}
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <ImageIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary="Course"
+              secondary={course}
+            />
+          </ListItem>
+          {/* <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <BeachAccessIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Vacation" secondary="July 20, 2014" />
+          </ListItem> */}
+        </Stack>
         {
           src === null
             ? (
                 'loading'
               )
             : (
-              <iframe src={src} height="100%" width="100%" />
+              <div className={styles.content}>
+                <div className={styles.open_in_new_tab_footnote}>
+                  <Link
+                    href={src}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Click here to open the file in a new tab.
+                  </Link>
+                </div>
+                <iframe src={src} height="100%" width="100%" />
+              </div>
               )
         }
-      </div>
-    </section>
-  );
+      </section>
+    );
+  };
 };
 
 export default Page;
