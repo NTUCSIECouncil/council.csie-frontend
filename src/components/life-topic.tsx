947387e668f5850119f62faabf7888ad7b87f69d
Yao1004 <changmingyao2014@gmail.com>
2025-03-19 22:27:58 +0800

    Revise component & finish leader

--- src/components/life-topic.tsx
+++ src/components/life-topic.tsx
@@ -3,31 +3,34 @@ const LifeTopic = ({
   subtopic,
   lecturer,
   author,
+  topicSize = 'text-5xl',
+  subtopicSize = 'text-3xl',
 }: {
   topic: string;
   subtopic?: string;
   lecturer?: string;
   author?: string;
+  topicSize?: string;
+  subtopicSize?: string;
 }): React.JSX.Element => {
   return (
     <>
-      <h1 className="text-5xl font-bold py-2">{topic}</h1>
+      <h1 className={`${topicSize} font-bold py-2`}>{topic}</h1>
       {subtopic && (
-        <h2 className="text-3xl font-bold py-1 text-[#BBB9BD]">{subtopic}</h2>
+        <h2 className={`${subtopicSize} font-bold py-1 text-[#BBB9BD]`}>{subtopic}</h2>
       )}
       <p className="font-bold mb-2">
         {lecturer && (
           <>
             教授：
-            <br />
-            {lecturer}
+            { lecturer }
           </>
         )}
+        <br />
         {author && (
           <>
             撰稿：
-            <br />
-            {author}
+            { author }
           </>
         )}
       </p>
