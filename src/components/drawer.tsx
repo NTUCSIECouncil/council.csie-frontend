0805e5598802cfd5af2591a98ae1eea150fcd672
miachou1225 <mia.tw2003@gmail.com>
2025-03-26 23:12:59 +0800

    feat: fix sidebar on pc, hide on mobile

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -1,25 +1,32 @@
 import { FaBars } from 'react-icons/fa6';
 const Drawer = ({
   choices,
+  selected,
 }: {
   choices: {
     value: string;
     description: string;
   }[];
+  selected: string;
 }): React.JSX.Element => {
   return (
-    <div className="drawer absolute top-20 left-0">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        <label htmlFor="my-drawer" className="btn gray-900 border-gray-500 border-2"><FaBars></FaBars></label>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 mt-20 p-4 font-bold text-xl">
-          {choices.map(choice => (
-            <li key={choice.value}><a href={choice.value} className="justify-center hover:text-white">{choice.description}</a></li>
-          ))}
-        </ul>
+    <div className="fixed top-20 left-0 md:relative md:top-0">
+      <div className="drawer md:drawer-open">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex md:hidden">
+          <label htmlFor="my-drawer" className="btn gray-900 border-gray-500 border-2 hover:bg-slate-600">
+            <FaBars></FaBars>
+            {/* <div className="font-bold text-lg ml-2">{selected}</div> */}
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 text-base-content p-4 font-bold text-xl border-2 rounded-2xl w-64 h-[70%]">
+            {choices.map(choice => (
+              selected == choice.description ? <li key={choice.value}><a href={choice.value} className="justify-center bg-gray-700 text-white border-b-2">{choice.description}</a></li> : <li key={choice.value}><a href={choice.value} className="justify-center hover:text-white">{choice.description}</a></li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/helpers/sidebar.tsx
+++ src/helpers/sidebar.tsx
@@ -1,10 +1,11 @@
 import Drawer from '@/components/drawer';
 import { SIDEBAR_OPTIONS, type SidebarOptionKeys } from '@/helpers/constants';
 
-export const sidebar = (key: SidebarOptionKeys) => {
+export const sidebar = (key: SidebarOptionKeys, selected: string) => {
   return (
     <Drawer
       choices={SIDEBAR_OPTIONS[key].choices}
+      selected={selected}
     />
   );
 };
