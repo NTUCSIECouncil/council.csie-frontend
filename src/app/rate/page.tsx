c4cd75deeaa4553d734567d2893889a77c1e7fc9
HyperSoWeak <hypersoweak@gmail.com>
2025-05-14 21:54:24 +0800

    feat: add new post button in home page

--- src/app/rate/page.tsx
+++ src/app/rate/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import Search from '@/components/search';
-import { renderFilter } from '@/helpers/filter';
 import searchRedirect from '@/helpers/search-redirect';
 import Background from './background';
 
@@ -12,23 +11,9 @@ const Page = (): React.JSX.Element => {
     <main className="flex flex-1 justify-center items-center">
       <Background />
       <div className="relative">
-        <p className="text-5xl font-bold my-4 tracking-widest text-center">課程評價網</p>
-        <form
-          action={searchRedirect(router, '/rate/filter-results')}
-          className="w-[500px] flex flex-col items-center"
-        >
-          <Search className="my-2 w-full" placeholder="輸入關鍵字" />
-          <div>
-            <div className="flex items-center gap-2 my-2">
-              <p className="text-lg">篩選：</p>
-              { renderFilter('courseGrade', 'grade') }
-              { renderFilter('courseCategory', 'type') }
-            </div>
-            {/* <div className="flex items-center gap-2 my-2">
-              <p className="text-lg">標籤：</p>
-              感覺做成搜尋的比較好，不知道 DCard 怎麼運作的
-            </div> */}
-          </div>
+        <p className="text-5xl font-bold my-6 tracking-widest text-center">課程評價網</p>
+        <form action={searchRedirect(router, '/rate/filter-results')} className="w-[500px] flex flex-col items-center -translate-x-8">
+          <Search className="my-2 w-full" placeholder="輸入關鍵字" hasAddButton={true} />
         </form>
       </div>
     </main>
