5a90beb669b1779d38ed30451fd2296c3769623e
miachou1225 <mia.tw2003@gmail.com>
2025-05-21 20:33:47 +0800

    fix(drawer): change sidebar responsive threshold to xl

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -34,10 +34,10 @@ const Drawer = ({ choices, selected }: Props): React.JSX.Element => {
   }, [storageKey]);
 
   return (
-    <div className="fixed top-20 left-0 lg:relative lg:top-0">
-      <div className="drawer lg:drawer-open">
+    <div className="fixed top-20 left-0 xl:relative xl:top-0">
+      <div className="drawer xl:drawer-open">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex lg:hidden">
+        <div className="drawer-content flex xl:hidden">
           <label
             htmlFor="my-drawer"
             className="btn gray-900 border-gray-500 border-2 hover:bg-slate-600"
@@ -54,12 +54,12 @@ const Drawer = ({ choices, selected }: Props): React.JSX.Element => {
           />
           <ul
             ref={listRef}
-            className="menu flex-nowrap overflow-y-scroll backdrop-blur-lg bg-opacity-30
-                       lg:bg-base-200 text-base-content p-4 font-bold text-xl
-                       lg:border-2 lg:rounded-2xl w-64 h-[100%] lg:h-[70%]"
+            className="menu flex-nowrap overflow-y-scroll backdrop-blur-xl bg-opacity-30
+                       xl:bg-base-200 text-base-content p-4 font-bold text-xl
+                       xl:border-2 xl:rounded-2xl w-64 h-[100%] xl:h-[70%]"
           >
 
-            <li className="mt-20 lg:mt-0 invisible" />
+            <li className="mt-20 xl:mt-0 invisible" />
 
             {choices.map(choice => (
               <li key={choice.value}>
