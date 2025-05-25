ef48588ae22791dc1708e320a58e32fcdce07a45
kxcathy <kxchen67@gmail.com>
2025-05-25 11:37:26 +0800

    feat: add link icon in life-information

--- src/components/life-link.tsx
+++ src/components/life-link.tsx
@@ -1,3 +1,5 @@
+import { IoMdLink } from 'react-icons/io';
+
 const LifeLink = ({
   content,
   href,
@@ -6,7 +8,10 @@ const LifeLink = ({
   href: string;
 }): React.JSX.Element => {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="text-nowrap py-1 px-4 rounded-xl bg-[#a167a5]/70 text-white hover:bg-[#a689b1] text-sm md:text-lg">{content}</a>
+    <a href={href} target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 text-nowrap py-1 px-4 rounded-xl bg-[#a167a5]/70 text-white hover:bg-[#a689b1] text-sm md:text-lg">
+      <IoMdLink className="-rotate-45" />
+      {content}
+    </a>
   );
 };
 
