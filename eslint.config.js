aa3caff9c941688a988e4483f8e0aa3ace820ba0
seantsao00 <seantsao00@gmail.com>
2025-09-03 02:36:41 +0000

    chore: migrate from ESLint sylistic to Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import tseslint from 'typescript-eslint';
 
 const compat = new FlatCompat({
@@ -13,20 +13,11 @@ export default tseslint.config(
   // non type-aware configs
   {
     ignores: ['dist', '.next', 'node_modules'],
-    plugins: {
-      '@stylistic': stylistic,
-    },
     extends: [
       eslint.configs.recommended,
       ...compat.config({ extends: ['next/core-web-vitals'] }),
-      stylistic.configs.customize({
-        semi: true,
-        braceStyle: '1tbs',
-      }),
+      eslintConfigPrettier,
     ],
-    rules: {
-      '@stylistic/max-statements-per-line': 'off',
-    },
   },
   // type-aware settings
   {
@@ -37,35 +28,24 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    settings: {
-      'import/parsers': {
-        '@typescript-eslint/parser': ['.ts', '.tsx'],
-      },
-      'import/resolver': {
-        // Load <rootdir>/tsconfig.json
-        typescript: true,
-        node: true,
-      },
-    },
     extends: [
       ...tseslint.configs.strictTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
       ...compat.config({ extends: ['next/typescript'] }),
+      eslintConfigPrettier, // used twice to suppress stylisticTypeChecked from tseslint
     ],
     rules: {
       'sort-imports': ['warn', { ignoreDeclarationSort: true }],
 
-      '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        { allowInterfaces: 'with-single-extends' },
+      ],
       '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
-      '@typescript-eslint/consistent-type-imports': ['warn', { fixStyle: 'inline-type-imports' }],
-
-      'import/order': ['warn', {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        alphabetize: { order: 'asc' },
-      }],
-      'import/export': 'error',
-      'import/no-duplicates': 'warn',
-      'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { fixStyle: 'inline-type-imports' },
+      ],
     },
   },
 );
